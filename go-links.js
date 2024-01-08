d7802f5bbdc2b815ef55694e96c219f8bfcd4e67
Michel Grolet <39418855+MichelGrolet@users.noreply.github.com>
2024-01-08 22:17:33 +0100

    Update go-links.js

--- go-links.js
+++ go-links.js
@@ -1,6 +1,7 @@
 const goLinks = {
 	'insta': 'https://www.instagram.com/michelgrolet.photographe/',
 	'add': 'https://github.com/MichelGrolet/go/edit/main/go-links.js',
+	'denaro': 'https://michelgrolet.notion.site/Denaro-a23a0dcbebce4f7c80cd7f17ad5c0078',
   };
   
   function redirect() {
