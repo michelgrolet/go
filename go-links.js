956dd5b6ec38d8e95a09e55702baaa02a664314b
Michel Grolet <39418855+MichelGrolet@users.noreply.github.com>
2024-03-23 00:39:54 +0100

    Update go-links.js

--- go-links.js
+++ go-links.js
@@ -3,6 +3,7 @@ const goLinks = {
 	'add': 'https://github.com/MichelGrolet/go/edit/main/go-links.js',
 	'denaro': 'https://michelgrolet.notion.site/Denaro-a23a0dcbebce4f7c80cd7f17ad5c0078',
 	'islam': 'https://drive.google.com/file/d/172MvsrDKxzeINuGkWMYg6a7D9RMvFkHR/view?usp=sharing',
+	'profil': 'https://photos.google.com/photo/AF1QipO0p4nNp3_mjnvLPjOtJRIo-gVXuNFMu7tIoqiE',
   };
   
   function redirect() {
