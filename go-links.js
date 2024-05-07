eee78f2e120d628a12c20689dad2b12356003ec6
Michel Grolet <39418855+MichelGrolet@users.noreply.github.com>
2024-05-07 11:22:13 +0200

    Update go-links.js

--- go-links.js
+++ go-links.js
@@ -5,6 +5,7 @@ const goLinks = {
 	'denaro': 'https://michelgrolet.notion.site/Denaro-a23a0dcbebce4f7c80cd7f17ad5c0078',
 	'islam': 'https://drive.google.com/file/d/172MvsrDKxzeINuGkWMYg6a7D9RMvFkHR/view?usp=sharing',
 	'profil': 'https://photos.google.com/photo/AF1QipO0p4nNp3_mjnvLPjOtJRIo-gVXuNFMu7tIoqiE',
+	'tarifs-photo': 'https://photographe.michelgrolet.fr/tarifs',
   };
   
   function redirect() {
