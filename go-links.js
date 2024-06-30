98a5035ff769e3cedceca765b7eea72269cc9330
Michel Grolet <39418855+MichelGrolet@users.noreply.github.com>
2024-06-30 17:09:32 +0200

    Update go-links.js

--- go-links.js
+++ go-links.js
@@ -8,6 +8,7 @@ const goLinks = {
 	// photo
 	'tarifs-photo': 'https://photographe.michelgrolet.fr/tarifs',
 	// perso misc
+	'calendar': 'https://calendar.google.com/calendar/embed?src=Z3JvbGV0QGdvb2dsZS5jb20&src=bDBwMHNjOW1obWtqOXBydHR2ZWplZnRicjFraWtiZGNAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=bWljaGVsZ3JvbGV0QGdtYWlsLmNvbQ&ctz=Europe/Paris'
 	'denaro': 'https://michelgrolet.notion.site/Denaro-a23a0dcbebce4f7c80cd7f17ad5c0078',
 	'islam': 'https://drive.google.com/file/d/172MvsrDKxzeINuGkWMYg6a7D9RMvFkHR/view?usp=sharing',
 	'profil': 'https://photos.google.com/photo/AF1QipO0p4nNp3_mjnvLPjOtJRIo-gVXuNFMu7tIoqiE',
