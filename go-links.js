2aa77ea9f33071ab5d5473fdaedf126d2b09c933
Michel Grolet <39418855+MichelGrolet@users.noreply.github.com>
2024-07-11 10:17:06 +0200

    Update go-links.js

--- go-links.js
+++ go-links.js
@@ -1,7 +1,8 @@
 const goLinks = {
 	// settings
 	'add': 'https://github.com/MichelGrolet/go/edit/main/go-links.js',
-	'new': 'https://github.com/MichelGrolet/go/edit/main/go-links.js',
+	'new': 'add',
+	'edit': 'add',
 	// socials
 	'insta': 'https://www.instagram.com/mi.grlt/',
 	'linkedin': 'https://www.linkedin.com/in/michel-grolet/',
@@ -9,11 +10,11 @@ const goLinks = {
 	'tarifs-photo': 'https://photographe.michelgrolet.fr/tarifs',
 	// perso misc
 	'calendar': 'https://calendar.google.com/calendar/embed?src=Z3JvbGV0QGdvb2dsZS5jb20&src=bDBwMHNjOW1obWtqOXBydHR2ZWplZnRicjFraWtiZGNAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=bWljaGVsZ3JvbGV0QGdtYWlsLmNvbQ&ctz=Europe/Paris',
-	'oo': 'https://calendar.google.com/calendar/embed?src=Z3JvbGV0QGdvb2dsZS5jb20&src=bDBwMHNjOW1obWtqOXBydHR2ZWplZnRicjFraWtiZGNAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=bWljaGVsZ3JvbGV0QGdtYWlsLmNvbQ&ctz=Europe/Paris',
+	'oo': 'calendar',
 	'denaro': 'https://michelgrolet.notion.site/Denaro-a23a0dcbebce4f7c80cd7f17ad5c0078',
 	'islam': 'https://drive.google.com/file/d/172MvsrDKxzeINuGkWMYg6a7D9RMvFkHR/view?usp=sharing',
-	'profil': 'https://photos.app.goo.gl/Bo7AL2mEYMSUpT4B9',
-	'profile': 'https://photos.google.com/photo/AF1QipO0p4nNp3_mjnvLPjOtJRIo-gVXuNFMu7tIoqiE',
+	'profil': 'https://photos.app.goo.gl/maDYfXWnCPSMFkTA8',
+	'profile': 'profil',
 	'edit-profile': 'https://docs.google.com/document/d/1Rw3iPQPOCEOUOB8BrgImoyNzDoYbZrSg_Z0aQieyMyg/edit?usp=sharing',
 	'edit-profil': 'edit-profile',
 	'abs': 'https://youtu.be/8PwoytUU06g'
