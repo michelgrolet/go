64bd7abc3be10be29b6f68a6a891ef342a4495ae
Michel Grolet <39418855+michelgrolet@users.noreply.github.com>
2024-10-01 11:36:57 +0200

    Update go-links.js

--- go-links.js
+++ go-links.js
@@ -14,7 +14,7 @@ const goLinks = {
 	// photo
 	'tarifs-photo': 'https://photographe.michelgrolet.fr/tarifs',
 	// lvx
-	'programme-meute-2024-2025':'https://drive.google.com/file/d/13B6UWid-775NhARnrLXX-P-1BL-paHYb/view?usp=sharing',
+	'programme-meute-2024-2025':'https://drive.google.com/file/d/1APaKU42Mcxv7Z04g7zIBCVu3f_zyULyJ/view?usp=sharing',
 	// perso misc
 	'site': 'https://www.michelgrolet.fr/',
 	'calendar': 'https://calendar.google.com/calendar/embed?src=Z3JvbGV0QGdvb2dsZS5jb20&src=bDBwMHNjOW1obWtqOXBydHR2ZWplZnRicjFraWtiZGNAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=bWljaGVsZ3JvbGV0QGdtYWlsLmNvbQ&src=bWljaGVsQGdldG91dC5zcG9ydA&ctz=Europe/Paris',
